4970e625ed4d605ce7e7ce708833c8910994151f
MusH <collinsmucheru@gmail.com>
2019-08-12 11:47:07 +0300

    IO.ANSI.color -> color text on the terminal

---
+++ cli/color.exs
@@ -0,0 +1,23 @@
+defmodule Color do
+  def green(text) do
+    IO.ANSI.green() <> text <> IO.ANSI.reset()
+  end
+
+  def red(text) do
+    IO.ANSI.red() <> text <> IO.ANSI.reset()
+  end
+
+  def code(code, text) do
+    IO.ANSI.color(code) <> text <> IO.ANSI.reset()
+  end
+end
+
+# demo
+IO.puts(Color.green("Text colored green"))
+IO.puts(Color.red("Text colored red"))
+
+0..255
+|> Enum.each(fn code ->
+  IO.puts(Color.code(code, "Code #{code}"))
+  Process.sleep(250)
+end)
