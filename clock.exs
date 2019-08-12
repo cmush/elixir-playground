84e64af5b02a7424b692fb693e11a486c427780d
MusH <collinsmucheru@gmail.com>
2019-08-12 09:15:49 +0300

    "Clock on the terminal" module with carriage return & IO.write

---
+++ clock.exs
@@ -0,0 +1,14 @@
+defmodule Clock do
+  def start do
+    time_string = Time.utc_now()
+    |> Time.truncate(:second)
+    |> Time.to_string()
+
+    IO.write("\r#{time_string}")
+
+    Process.sleep(1_000)
+    start()
+  end
+end
+
+Clock.start()
